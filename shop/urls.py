479ab51e9cdc10ce6e7c1fc5e23bbc3d8f1a2667
Jerson Rafael <jersonrafael800@gmail.com>
2024-04-28 12:12:24 -0400

    Font and another changes for fix styles problems

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('products/', products ,name='products')
+    path('products/', products ,name='products'),
+    path('products/<int:pk>', category_products ,name='category_products'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from products.models import *
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -16,4 +17,17 @@ def products(request):
       'categorys':categorys_model,
       'products':products_model
     }
+    if request.method == 'POST':
+      category_id = request.POST
+      print(category_id)
+      return redirect(f'products/{category_id}')
     return render(request, template_name='shop/products.html', context=context)
+
+def category_products(request,pk):
+  products_model = product.objects.filter(category=pk)
+  categorys_model = category.objects.all()
+  context = {
+    'categorys':categorys_model,
+    'products':products_model
+  }
+  return render(request,template_name='shop/filter_products.html',context=context)
