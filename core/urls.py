f5b4a762557d739d6020d93f0c3981e98480923a
jerson <jrrvgamer@gmail.com>
2024-06-17 10:21:05 -0400

    Deploy features added

--- core/urls.py
+++ core/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('', include('shop.urls')),
 
     path("__reload__/", include("django_browser_reload.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
